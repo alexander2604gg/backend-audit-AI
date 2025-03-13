3ca7492839a1d4efdb7dce36fd1fa58049d2e5fe
Alexander Saul <alexandersaul2604@gmail.com>
2025-03-12 21:38:16 -0500

    first commit

---
+++ src/test/java/com/portafolio/audit_AI/AuditAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portafolio.audit_AI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuditAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
