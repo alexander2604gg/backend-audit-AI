4cf706a5af5dd435c960fd8c25daee70db59e03f
Alexander Saul <alexandersaul2604@gmail.com>
2025-03-15 17:58:18 -0500

    feat: modificar enpoint para una respuesta

--- src/main/java/com/portafolio/auditai/service/impl/ChatServiceImpl.java
+++ src/main/java/com/portafolio/auditai/service/impl/ChatServiceImpl.java
@@ -23,6 +23,10 @@ public class ChatServiceImpl implements ChatService {
                 "para identificar inconsistencias, anomalías o posibles fraudes. Examina patrones sospechosos, discrepancias en los" +
                 " datos y cualquier indicio de manipulación contable. Presenta tu análisis con una justificación clara y basada en " +
                 "datos.");
+        Message messageResponse = new Message("system", "\"Quiero que analices los estados financieros proporcionados y detectes posibles irregularidades. Tu respuesta debe incluir dos partes:  \n" +
+                "\n" +
+                "1. **Observación general:** Un análisis detallado sobre posibles inconsistencias, riesgos financieros o anomalías en los datos.  \n" +
+                "2. **JSON para dashboard:** Un JSON estructurado con métricas clave, indicadores de riesgo y observaciones resumidas en un formato que pueda ser utilizado para visualizar en un dashboard.  ");
         Message messageFromUser = new Message("user" ,userInput);
         DeepSeekRequestDto request = new DeepSeekRequestDto(
                 "deepseek-chat",
